ca27610e8c8af84e150b2e8b67107e285a416b85
smiglu <jkb.smigielski@gmail.com>
2017-05-14 18:59:09 +0200

    First working version.

--- config/karma/default.js
+++ config/karma/default.js
@@ -66,8 +66,7 @@ module.exports = function( config ) {
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: [
-			'Chrome',
-			'Firefox'
+			'Chrome'
 		],
 
 
